ffffd00d91bd3450341c98898890e3fa2605641b
Rifkynur <ythrifky619@gmail.com>
2024-10-21 12:40:38 +0700

    fix bug porto page

--- src/Component/portofilio/PortofolioCard.jsx
+++ src/Component/portofilio/PortofolioCard.jsx
@@ -3,7 +3,7 @@ import PortofolioLinks from "./PortofolioLinks";
 
 const PortofolioCard = ({ data }) => {
   return (
-    <div className="w-[44%] pb-2 rounded-xl border-[2px] border-black shadow-cardShadowSmall relative overflow-hidden group shadow-slate-900 dark:bg-slate-600 md:shadow-cardShadowBig dark:md:shadow-darkShadowBig lg:w-[30%] dark:shadow-darkShadowSmall">
+    <div className="w-full pb-2 h-full rounded-xl border-[2px] border-black shadow-cardShadowSmall relative overflow-hidden group shadow-slate-900 dark:bg-slate-600 md:shadow-cardShadowBig dark:md:shadow-darkShadowBig dark:shadow-darkShadowSmall">
       <PortofolioLinks data={data} />
       <PortofolioDesc img={data.img} tool={data.tool} title={data.title} />
     </div>
--- src/Component/portofilio/PortofolioDesc.jsx
+++ src/Component/portofilio/PortofolioDesc.jsx
@@ -1,17 +1,17 @@
 import EachUtils from "../../util/EachUtils";
 const PortofolioDesc = ({ img, title, tool }) => {
   return (
-    <div>
+    <div className="h-full flex flex-col justify-between items-start">
       <div className="w-full h-[60px] object-cover md:h-[100px] lg:h-[150px]">
-        <img src={img} alt="dentist" className=" w-full h-14 md:h-24 lg:h-40" />
+        <img src={img} alt="dentist" className=" w-full h-[70px] md:h-24 lg:h-40" />
       </div>
       <div className="p-1 flex flex-col justify-between md:p-2 md:h-[75px] lg:h-[110px]">
         <h3 className="text-[10px] font-bold mt-1 max-w-full lg:text-lg">{title}</h3>
-        <div className="flex flex-w gap-1 mt-2">
+        <div className="flex flex-wrap gap-1 mt-2">
           <EachUtils
             of={tool}
             render={(data) => {
-              return <span className=" bg-light dark:bg-dark text-black border font-semibold text-[6px] py-[2px] px-1 rounded-md border-black  md:text-[10px] lg:text-sm">{data}</span>;
+              return <span className="bg-light dark:bg-dark text-black border font-semibold text-[6px] py-[2px] px-1 rounded-md border-black  md:text-[10px] lg:text-sm">{data}</span>;
             }}
           />
         </div>
