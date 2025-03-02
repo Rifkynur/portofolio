d3c1b412aa45db2e4dde88517381644606d29f0d
Rifkynur <ythrifky619@gmail.com>
2025-03-02 14:17:44 +0700

    add laravel skill

--- src/pages/skillPage.jsx
+++ src/pages/skillPage.jsx
@@ -11,12 +11,37 @@ function SkillPage() {
   return (
     <div className="main-content ">
       <Title title="My Skill" />
+      <h3 className="text-sm w-full mt-5 lg:mt-10 font-semibold lg:text-base lg:py-2">Programming Language</h3>
       <div
-        className="grid grid-cols-2 gap-5 text-center mt-5 lg:mt-10
+        className="grid grid-cols-2 gap-5 text-center 
       lg:grid-cols-3 lg:gap-12 lg:px-10 "
       >
         <EachUtils
-          of={skill}
+          of={skill.filter((item) => item.type == "programmingLanguage")}
+          render={(data) => {
+            return <CardSkill img={data.img} title={data.title} />;
+          }}
+        />
+      </div>
+      <h3 className="text-sm w-full mt-5 lg:mt-10 font-semibold lg:text-base lg:py-2">Framework</h3>
+      <div
+        className="grid grid-cols-2 gap-5 text-center 
+      lg:grid-cols-3 lg:gap-12 lg:px-10 "
+      >
+        <EachUtils
+          of={skill.filter((item) => item.type == "framework")}
+          render={(data) => {
+            return <CardSkill img={data.img} title={data.title} />;
+          }}
+        />
+      </div>
+      <h3 className="text-sm w-full mt-5 lg:mt-10 font-semibold lg:text-base lg:py-2">Tool</h3>
+      <div
+        className="grid grid-cols-2 gap-5 text-center 
+      lg:grid-cols-3 lg:gap-12 lg:px-10 "
+      >
+        <EachUtils
+          of={skill.filter((item) => item.type == "tool")}
           render={(data) => {
             return <CardSkill img={data.img} title={data.title} />;
           }}
